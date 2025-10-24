377240b6c5ffaab8e893b15a7d72da3136789e60
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2025-10-23 20:17:14 -0400

    Creacion del componente para los titulos de secciones

---
+++ app/src/main/java/dev/boni/techhelpdesk/ui/components/SectionTitle.kt
@@ -0,0 +1,43 @@
+package dev.boni.techhelpdesk.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.boni.techhelpdesk.ui.theme.TechHelpDeskTheme
+
+/**
+ * Un componente de texto estandarizado para los títulos de las secciones.
+ *
+ * @param text El texto del título a mostrar.
+ * @param modifier Un modificador opcional.
+ */
+@Composable
+fun SectionTitle(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        modifier = modifier.padding(bottom = 16.dp),
+        style = MaterialTheme.typography.titleLarge.copy(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+        ),
+        color = MaterialTheme.colorScheme.onSurface,
+        lineHeight = 24.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SectionTitlePreview() {
+    TechHelpDeskTheme {
+        SectionTitle(text = "Resumen de tickets")
+    }
+}
