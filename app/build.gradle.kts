da5cfc5bcc8d8e6c10e622e90c9a0a2dc8b99ebe
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2025-11-15 00:00:15 -0400

    Configuracion de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias (libs.plugins.google.services)
 }
 
 android {
@@ -40,15 +41,10 @@ android {
 }
 
 dependencies {
-    // Navegación en Compose
-    implementation(libs.androidx.navigation.compose)
-    // Icons
-    implementation(libs.androidx.compose.material.icons.extended)
-    // Animaciones
-    implementation(libs.androidx.compose.animation)
-
+    implementation(libs.androidx.navigation.compose) // Navegación en Compose
+    implementation(libs.androidx.compose.material.icons.extended) // Icons
+    implementation(libs.androidx.compose.animation) // Animaciones
     implementation(libs.androidx.core.splashscreen)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -57,11 +53,19 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(platform(libs.google.firebase.bom))
+    implementation(libs.google.firebase.auth.ktx)
+    implementation(libs.google.firebase.firestore.ktx)
+    implementation(libs.kotlinx.coroutines.playservices)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
